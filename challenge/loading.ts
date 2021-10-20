b374c954bf1837bb1ff247403881e136f292e047
Charlie Kim <dh77dh77@gmail.com>
2021-10-20 15:07:22 +0900

    loading state challenge

--- challenge/loading.ts
+++ challenge/loading.ts
@@ -3,24 +3,40 @@
    * Print Loading State
    */
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  function printLoginState(resourceLoadState: ResourceLoadState) {
+    switch (resourceLoadState.state) {
+      case "loading":
+        console.log(`👀 ${resourceLoadState.state}...`);
+        break;
+      case "success":
+        console.log(`😃 ${resourceLoadState.response.body}`);
+        break;
+      case "fail":
+        console.log(`😱 ${resourceLoadState.reason}`);
+        break;
+      default:
+        throw new Error(`unknown state: ${resourceLoadState}`);
+    }
+  }
+
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
 }
