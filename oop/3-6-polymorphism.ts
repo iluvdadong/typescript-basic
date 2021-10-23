3256cee3d8b93313137e6a4068706c220d9b13ae
Charlie Kim <dh77dh77@gmail.com>
2021-10-23 21:31:48 +0900

    polymorphism

--- oop/3-6-polymorphism.ts
+++ oop/3-6-polymorphism.ts
@@ -1,8 +1,8 @@
 {
   type CoffeeCup = {
     shots: number;
-    hasMilk?: boolean;
-    hasSugar?: boolean;
+    hasMilk?: boolean; // 옵셔널!
+    hasSugar?: boolean; // 옵셔널! sugar가 있을수도 있고 없을수도 있게
   };
 
   interface CoffeeMaker {
@@ -23,25 +23,25 @@
 
     fillCoffeeBeans(beans: number) {
       if (beans < 0) {
-        throw new Error('value for beans should be greater than 0');
+        throw new Error("value for beans should be greater than 0");
       }
       this.coffeeBeans += beans;
     }
 
     clean() {
-      console.log('cleaning the machine...🧼');
+      console.log("cleaning the machine...🧼");
     }
 
     private grindBeans(shots: number) {
       console.log(`grinding beans for ${shots}`);
       if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
-        throw new Error('Not enough coffee beans!');
+        throw new Error("Not enough coffee beans!");
       }
       this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
     }
 
     private preheat(): void {
-      console.log('heating up... 🔥');
+      console.log("heating up... 🔥");
     }
 
     private extract(shots: number): CoffeeCup {
@@ -64,7 +64,7 @@
       super(beans);
     }
     private steamMilk(): void {
-      console.log('Steaming some milk... 🥛');
+      console.log("Steaming some milk... 🥛");
     }
     makeCoffee(shots: number): CoffeeCup {
       const coffee = super.makeCoffee(shots);
@@ -77,8 +77,12 @@
   }
 
   class SweetCoffeeMaker extends CoffeeMachine {
+    private addingSugar(): void {
+      console.log("Adding Sugar... ✨");
+    }
     makeCoffee(shots: number): CoffeeCup {
       const coffee = super.makeCoffee(shots);
+      this.addingSugar();
       return {
         ...coffee,
         hasSugar: true,
@@ -86,16 +90,23 @@
     }
   }
 
+  // CoffeeMaker라는 interface 타입으로도 정의가 가능하고
+  // CoffeeMachine이라는 구현부인 class 타입으로도 정의가 가능하다.
+  // 그대신 인터페이스 호출시 공개해놓은 makeCoffee() 함수만 호출 가능하다.
   const machines: CoffeeMaker[] = [
     new CoffeeMachine(16),
-    new CaffeLatteMachine(16, '1'),
+    new CaffeLatteMachine(16, "1"),
     new SweetCoffeeMaker(16),
     new CoffeeMachine(16),
-    new CaffeLatteMachine(16, '1'),
+    new CaffeLatteMachine(16, "1"),
     new SweetCoffeeMaker(16),
   ];
-  machines.forEach(machine => {
-    console.log('-------------------------');
+
+  // 다형성의 장점!
+  // 내부적으로 구현된 다양한 class들이 한가지 인터페이스를 구현하거나 동일한 부모를 상속받을 때
+  // 공통된 함수 호출 가능 : 약속된 한가지의 api 호출 가능
+  machines.forEach((machine) => {
+    console.log("-------------------------");
     machine.makeCoffee(1);
   });
 }
