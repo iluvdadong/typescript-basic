c86f547a427cc766aed094ef8ae791636381f272
Charlie Kim <dh77dh77@gmail.com>
2021-10-24 15:55:08 +0900

    abstact

--- oop/3-8-abstract.ts
+++ oop/3-8-abstract.ts
@@ -1,4 +1,7 @@
 {
+  /**
+   * 커플링을 피하자! Class가 서로 뭐하는지 모르는게 좋다.
+   */
   type CoffeeCup = {
     shots: number;
     hasMilk?: boolean;
@@ -19,27 +22,32 @@
 
     fillCoffeeBeans(beans: number) {
       if (beans < 0) {
-        throw new Error('value for beans should be greater than 0');
+        throw new Error("value for beans should be greater than 0");
       }
       this.coffeeBeans += beans;
     }
 
     clean() {
-      console.log('cleaning the machine...🧼');
+      console.log("cleaning the machine...🧼");
     }
 
     private grindBeans(shots: number) {
       console.log(`grinding beans for ${shots}`);
       if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
-        throw new Error('Not enough coffee beans!');
+        throw new Error("Not enough coffee beans!");
       }
       this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
     }
 
     private preheat(): void {
-      console.log('heating up... 🔥');
+      console.log("heating up... 🔥");
     }
 
+    // abastract 구현 X
+    // interface처럼 구현하고, 각각 구현하는 클래스마다 달라져야하는 함수만
+    // 구현하는 class에서 구현하면 됨
+    // 안전하게 의도한대로 공통적인 기능을 수행하고, 상속받은 클래스에만 이거 꼭 구현해야해! 할 수 있음
+    // 그래도 가능하면 깊은 상속보다는 컴포지션 이용하라
     protected abstract extract(shots: number): CoffeeCup;
 
     makeCoffee(shots: number): CoffeeCup {
@@ -54,9 +62,10 @@
       super(beans);
     }
     private steamMilk(): void {
-      console.log('Steaming some milk... 🥛');
+      console.log("Steaming some milk... 🥛");
     }
 
+    // abstract extract 메소드 구현
     protected extract(shots: number): CoffeeCup {
       this.steamMilk();
       return {
@@ -76,13 +85,13 @@
   }
 
   const machines: CoffeeMaker[] = [
-    new CaffeLatteMachine(16, '1'),
+    new CaffeLatteMachine(16, "1"),
     new SweetCoffeeMaker(16),
-    new CaffeLatteMachine(16, '1'),
+    new CaffeLatteMachine(16, "1"),
     new SweetCoffeeMaker(16),
   ];
-  machines.forEach(machine => {
-    console.log('-------------------------');
+  machines.forEach((machine) => {
+    console.log("-------------------------");
     machine.makeCoffee(1);
   });
 }
